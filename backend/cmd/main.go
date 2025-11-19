67399f533460a5a2f60560b9d9211e94e1f83176
LeoninCS <xianchaoqian@gmail.com>
2025-11-20 00:17:19 +0800

    fix:解决了服务器地址监听问题

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -14,6 +14,8 @@ func main() {
 	// 所有后端路由统一加 /api
 	mux.Handle("/api/", http.StripPrefix("/api", h))
 
-	log.Println("listen on 127.0.0.1:8080")
-	http.ListenAndServe("127.0.0.1:8080", mux)
+	log.Println("listen on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
